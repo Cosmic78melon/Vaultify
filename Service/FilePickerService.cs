01de63a656264a7899e40c240df209c26799561f
Tamjid <thadol881@gmail.com>
2026-04-29 23:15:21 +0600

    added missing folders in github and refactored the code

---
+++ Service/FilePickerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+
+namespace Password_Manager.Service;
+
+public class FilePickerService(Func<TopLevel?> toplevel)
+{
+    public async Task<string?> SaveFile(
+        string masterPass)
+    {
+        var topLevel = toplevel();
+
+        if (topLevel == null)
+            return null;
+
+        var folders = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions()
+        {
+            AllowMultiple = false,
+            Title = "Select a Folder"
+        });
+        var path = folders.FirstOrDefault();
+        return path.TryGetLocalPath();
+    }
+}
---
+++ Service/IPythonAPI.cs
@@ -0,0 +1,18 @@
+﻿namespace Password_Manager.Service
+{
+    public interface IPythonAPI
+    {
+        public string Generate_password(string siteName = null!, int length = 12);
+        public string CustomeGen(string siteName = null!, int length = 12, bool has_Letter = true, bool hasNum = true, bool hasPunc = true);
+        public dynamic PassswordCheck(string password = null!);
+        public dynamic show_all_data(string password);
+        public bool isAuthenticated(string password);
+        public bool isNewUser();
+        public bool register(string userName, string password);
+        public bool addCredentials(string masterpassword, string siteName, string userName = "Nothing", string password = "Noting", string message = "unknown", string catagory = "unknown", bool favourite = false);
+        public dynamic card_Data(string masterpassword);
+        public dynamic favData(string masterpassword);
+        public dynamic statusdata(string masterpassword);
+        public bool ExportVault(string masterPass, string Command, dynamic file_path, bool isDec);
+    }
+}
